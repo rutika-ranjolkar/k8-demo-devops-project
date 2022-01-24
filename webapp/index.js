8f7e1e854914fc70cc1508871b6fdf6caf718c78
Rutika <rutika.araokar@gmail.com>
2022-01-23 20:21:20 -0500

    webapp

---
+++ webapp/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const dns = require('dns')
+const os = require('os');
+const app = express()
+const port = 8080
+
+dns.lookup(os.hostname(), function(err, result) {
+  console.log("My ip address is: " + result);
+  app.get('/', (req, res) => {
+    res.send('IP: ' + result);
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  })
+})
